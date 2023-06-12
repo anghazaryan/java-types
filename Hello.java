484987521cd255a47a8b9e3f2dc49c6765b5cf99
anghazaryan <anahit.ghazaryan91@gmail.com>
2023-06-12 19:40:43 +0400

    first commit

---
+++ Hello.java
@@ -0,0 +1,22 @@
+class Hello {
+
+public static void main(String[] args){
+
+String firstName = "Anahit";
+String lastName = "Ghazaryan";
+
+System.out.println("My first name is" + " " + firstName + " " + "and my last name is" + " " + lastName);
+
+int firstNumber = 25;
+double secondNumber = 25.80d;
+double firstResult = firstNumber + secondNumber;
+System.out.println(firstResult);
+
+byte thirdNumber = 45;
+float forthNumber = 36.88f;
+float secondResult = thirdNumber * forthNumber;
+System.out.println(secondResult);
+
+  }	
+
+}
